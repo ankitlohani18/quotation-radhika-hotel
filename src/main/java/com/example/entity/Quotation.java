0ee8c6f352815335138a03ef4b1d2e65247dd5f5
ankitlohani09 <alohani.evision@gmail.com>
2024-12-17 10:45:15 +0530

    code refactor

--- src/main/java/com/example/entity/Quotation.java
+++ src/main/java/com/example/entity/Quotation.java
@@ -4,13 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
 
 @Entity
-@RequiredArgsConstructor
+@AllArgsConstructor
 @NoArgsConstructor
 public class Quotation {
     @Id
