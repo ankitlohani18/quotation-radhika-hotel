810275f7c0e88df6d5ec593041de3a4a4a871ede
ankitlohani09 <alohani.evision@gmail.com>
2024-12-20 18:54:19 +0530

    Refactor all code . 20 dec

--- src/main/java/com/example/entity/Quotation.java
+++ src/main/java/com/example/entity/Quotation.java
@@ -19,6 +19,7 @@ public class Quotation {
     private String address;
     private String subject;
     private String thankYou;
+    private String onOffGrid;
 
     private String proposedSiteName;
     private String location;
@@ -54,29 +55,11 @@ public class Quotation {
     private String totalAmountPayable;
     private String directSubsidyBenefit;
 
-    public String getPlantSize() {
-        return plantSize;
-    }
+    private String solarPVModulesBrand;
+    private String solarInverterBrand;
+    private String otherBrand;
 
-    public void setPlantSize(String plantSize) {
-        this.plantSize = plantSize;
-    }
 
-    public String getTotalAmountPayable() {
-        return totalAmountPayable;
-    }
-
-    public void setTotalAmountPayable(String totalAmountPayable) {
-        this.totalAmountPayable = totalAmountPayable;
-    }
-
-    public String getDirectSubsidyBenefit() {
-        return directSubsidyBenefit;
-    }
-
-    public void setDirectSubsidyBenefit(String directSubsidyBenefit) {
-        this.directSubsidyBenefit = directSubsidyBenefit;
-    }
 
     public Long getId() {
         return id;
@@ -132,6 +115,14 @@ public class Quotation {
 
     public void setThankYou(String thankYou) {
         this.thankYou = thankYou;
+    }
+
+    public String getOnOffGrid() {
+        return onOffGrid;
+    }
+
+    public void setOnOffGrid(String onOffGrid) {
+        this.onOffGrid = onOffGrid;
     }
 
     public String getProposedSiteName() {
@@ -340,6 +331,54 @@ public class Quotation {
 
     public void setNetMeterQty(String netMeterQty) {
         this.netMeterQty = netMeterQty;
+    }
+
+    public String getPlantSize() {
+        return plantSize;
+    }
+
+    public void setPlantSize(String plantSize) {
+        this.plantSize = plantSize;
+    }
+
+    public String getTotalAmountPayable() {
+        return totalAmountPayable;
+    }
+
+    public void setTotalAmountPayable(String totalAmountPayable) {
+        this.totalAmountPayable = totalAmountPayable;
+    }
+
+    public String getDirectSubsidyBenefit() {
+        return directSubsidyBenefit;
+    }
+
+    public void setDirectSubsidyBenefit(String directSubsidyBenefit) {
+        this.directSubsidyBenefit = directSubsidyBenefit;
+    }
+
+    public String getSolarPVModulesBrand() {
+        return solarPVModulesBrand;
+    }
+
+    public void setSolarPVModulesBrand(String solarPVModulesBrand) {
+        this.solarPVModulesBrand = solarPVModulesBrand;
+    }
+
+    public String getSolarInverterBrand() {
+        return solarInverterBrand;
+    }
+
+    public void setSolarInverterBrand(String solarInverterBrand) {
+        this.solarInverterBrand = solarInverterBrand;
+    }
+
+    public String getOtherBrand() {
+        return otherBrand;
+    }
+
+    public void setOtherBrand(String otherBrand) {
+        this.otherBrand = otherBrand;
     }
 }
 
